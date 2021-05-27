74319ae4fad14c392a43c95c735ce5b27a7d14f5
Julien-Wiegandt <julienwiegandt@gmail.com>
2021-05-27 12:16:58 +0200

    experience done

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -3,13 +3,46 @@
     <div class="title">
       <img src="../assets/experience.png" alt="experience title" />
     </div>
-    <div class="content"></div>
+    <div class="content">
+      <ExperienceCard
+        image="experience-agence-regionale-de-sante.png"
+        title="Responsable d’un numéro vert"
+        company="Agence régionale de santé - Auxiliaire de vacance"
+        date="Juillet 2019 - 1 mois"
+        location="Montpellier, Occitanie, France"
+        description="Responsable d’un numéro vert concernant des taux d’arsenic élevé dans l’Aude."
+      />
+      <span class="line" />
+      <ExperienceCard
+        image="experience-aip-primeca.png"
+        title="Administrateur de parc machine"
+        company="AIP-PRIMECA - Stage"
+        date="Avril 2019 - 2 mois"
+        location="Toulouse, Occitanie, France"
+        description="Chargé de la gestion d’un parc machine : déploiement de FOG, DHCP, serveur dédié."
+        margin-top="18"
+      />
+      <span class="line" />
+      <ExperienceCard
+        image="experience-societe-generale.png"
+        title="Auxiliaire de vacance"
+        company="Société Générale - Job dété"
+        date="Juillet 2019 - 1 mois"
+        location="Montpellier, Occitanie, France"
+        description="Prise en charge des clients en irrégularités, vérification de documents juridiques."
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import ExperienceCard from "./ExperienceCard";
+
 export default {
   name: "Experience",
+  components: {
+    ExperienceCard,
+  },
 };
 </script>
 
@@ -23,5 +56,17 @@ export default {
 
 .title img {
   width: 700px;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.line {
+  width: 400px;
+  height: 1px;
+  background-color: #b1b1b1;
 }
 </style>
---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="experience-card">
+    <img :src="require(`@/assets/${image}`)" :style="marginStyle" alt="company image" />
+    <div class="text">
+      <p class="title">{{ title }}</p>
+      <p class="info">{{ company }}</p>
+      <p class="info">{{ date }}</p>
+      <p class="info">{{ location }}</p>
+      <p class="description">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExperienceCard",
+  props: {
+    image: { type: String, required: true },
+    title: { type: String, required: true },
+    company: { type: String, required: true },
+    date: { type: String, required: true },
+    location: { type: String, required: true },
+    description: { type: String, required: true },
+    marginTop: { type: Number, required: false, default: 30 },
+  },
+  computed: {
+    marginStyle() {
+      return {
+        "margin-top": `${this.marginTop}px`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.experience-card {
+  /* background-color: #fafafa; */
+  background: #f7f7f7;
+  width: 700px;
+  height: 200px;
+  margin: 40px;
+  display: flex;
+  justify-content: space-around;
+}
+
+img {
+  height: 70px;
+  width: 70px;
+  object-fit: contain;
+  margin: 20px 25px 0px 25px;
+}
+
+.text {
+  text-align: left;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.title {
+  font-family: "Roboto";
+  font-weight: 500;
+  margin: 0;
+}
+
+.info {
+  font-family: "Roboto";
+  font-weight: 400;
+  color: #3e3e3e;
+  margin: 0;
+}
+
+.description {
+  font-family: "Roboto";
+  font-weight: 400;
+}
+</style>
