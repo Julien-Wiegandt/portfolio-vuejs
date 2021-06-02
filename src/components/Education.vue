f3c8c10134bce818aabe0ee100a70ada9f1d1e42
Julien-Wiegandt <julienwiegandt@gmail.com>
2021-06-02 22:58:01 +0200

    link updated

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -8,7 +8,7 @@
         diploma="Ingénieur en Informatique et Gestion"
         date="2019-2022"
         description="Je suis en 4ème année d’école d’ingénieur dans la filière Informatique et Gestion. J’y apprend à réfléchir avant d’agir, à résoudre des problèmes en général mais surtout à accroitre mes connaissances dans l’informatique ainsi que dans la gestion d’entreprise."
-        link="https://www.polytech.umontpellier.fr/"
+        link="https://www.polytech.umontpellier.fr/formation/cycle-ingenieur/informatique-et-gestion"
       />
       <img src="../assets/icon-arrow-up.png" />
       <EducationCard
@@ -17,7 +17,7 @@
         diploma="DUT en Informatique et Gestion"
         date="2017-2019"
         description="J'ai obtenu un DUT en informatique et gestion à Blagnac, j'y ai appris les principaux langages de programmation en participant à de nombreux projets d'équipe, j'ai été admis en classe anglais ce qui m'a permis de suivre certains cours en anglais."
-        link="https://www.iut-blagnac.fr/fr"
+        link="https://www.iut-blagnac.fr/fr/formations/dut-informatique"
       />
     </div>
   </div>
