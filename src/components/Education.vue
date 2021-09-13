2e3b2c44e0ade8f7390c36927e2043cb16c6c415
Julien-Wiegandt <julienwiegandt@gmail.com>
2021-09-12 23:42:22 -0400

    Sherbrooke added + Projects deleted

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -3,11 +3,20 @@
     <Banner title="Formation" image="formation.png" />
     <div class="content">
       <EducationCard
+        title="Université de Sherbrooke"
+        image="formation-universite-sherbrooke-2.jpg"
+        diploma="Maîtrise en Génie logiciel"
+        date="2021-2022"
+        description="Je suis en double diplôme à Sherbrooke au Canada. J’y approfondis mes connaissances en génie logiciel pour me spécialiser dans ce domaine. Cette expérience internationale est une super opportunité de découvrir la culture, le travail en entreprise et les beaux paysages canadiens."
+        link="https://www.usherbrooke.ca/admission/programme/651/maitrise-en-genie-logiciel"
+      />
+      <img src="../assets/icon-arrow-up.png" />
+      <EducationCard
         title="Polytech Montpellier"
         image="formation-polytech.png"
         diploma="Ingénieur en Informatique et Gestion"
         date="2019-2022"
-        description="Je suis en 4ème année d’école d’ingénieur dans la filière Informatique et Gestion. J’y apprend à réfléchir avant d’agir, à résoudre des problèmes en général mais surtout à accroitre mes connaissances dans l’informatique ainsi que dans la gestion d’entreprise."
+        description="Je suis en 5ème année d’école d’ingénieur dans la filière Informatique et Gestion. J’y apprends à réfléchir avant d’agir, à résoudre des problèmes en général mais surtout à accroitre mes connaissances dans l’informatique ainsi que dans la gestion d’entreprise."
         link="https://www.polytech.umontpellier.fr/formation/cycle-ingenieur/informatique-et-gestion"
       />
       <img src="../assets/icon-arrow-up.png" />
@@ -16,7 +25,7 @@
         image="formation-iut-blagnac.png"
         diploma="DUT en Informatique et Gestion"
         date="2017-2019"
-        description="J'ai obtenu un DUT en informatique et gestion à Blagnac, j'y ai appris les principaux langages de programmation en participant à de nombreux projets d'équipe, j'ai été admis en classe anglais ce qui m'a permis de suivre certains cours en anglais."
+        description="J'ai obtenu un DUT en informatique et gestion à Blagnac, j'y ai appris les principaux langages de programmation en participant à de nombreux projets d'équipe, j'ai été admis en 'classe anglais' ce qui m'a permis de suivre certains cours en anglais."
         link="https://www.iut-blagnac.fr/fr/formations/dut-informatique"
       />
     </div>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,42 +3,43 @@
     <Banner title="Projets" image="projects.png" />
     <div class="content">
       <div class="column__left">
-        <ProjectCard
+        <!-- <ProjectCard
           title="Todo React"
           description="Simple To Do list in React Redux connected to a DNSP Backend API."
           image="project-todo-react-redux.png"
           link="https://github.com/Julien-Wiegandt/todo-react-redux"
-        />
+        /> -->
       </div>
       <div class="column__center">
-        <ProjectCard
+        <!-- <ProjectCard
           title="Todo Angular"
           description="Simple To Do list in Angular connected to the DNSP Backend API."
           image="project-todo-angular.png"
           margin-top="120"
           link="https://github.com/Julien-Wiegandt/todo-angular"
-        />
+        /> -->
       </div>
       <div class="column__right">
-        <ProjectCard
+        <!-- <ProjectCard
           title="Todo DNSP"
           description="Simple To Do backend API using Docker - Node - Express - Sequelize - PostGres."
           image="project-todo-dnsp.png"
           link="https://github.com/Julien-Wiegandt/todo-dnsp"
-        />
+        /> -->
       </div>
     </div>
+    <Banner title="Work In Progress" image="work-in-progress.png" />
   </div>
 </template>
 
 <script>
-import ProjectCard from "./ProjectCard";
+// import ProjectCard from "./ProjectCard";
 import Banner from "./Banner";
 
 export default {
   name: "Projects",
   components: {
-    ProjectCard,
+    // ProjectCard,
     Banner,
   },
 };
